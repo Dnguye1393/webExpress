04ed445ccc4cc5b0f83a3a72eab012523673764e
David Nguyen <dnguyen1393@gmail.com>
2017-04-25 19:12:55 -0700

    Adding post and Get specific requests

--- app/routes/lights.js
+++ app/routes/lights.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 //var lightSchema = require('./models/lightSchema');
 var lightSchema = require('../models/lightSchema');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.route('/')
+.get(function(req, res, next) {
   //retrieve all lights from Monogo
     lightSchema.find({}, function (err, lights) {
           if (err) {
@@ -15,6 +16,60 @@ router.get('/', function(req, res, next) {
                 //JSON response will show all blobs in JSON format
                 json: function(){
                     res.json(lights);
+                }
+            });
+          }
+    });
+})
+.post( function(req, res, next) {
+  //retrieve all lights from Monogo
+    var response = {};
+    var newLight = new lightSchema();
+    newLight.pin = req.body.pin;
+    newLight.type = req.body.type;
+    newLight.info = req.body.info;
+    newLight.save(function(err){
+    // save() will run insert() command of MongoDB.
+    // it will add new data in collection.
+    if(err) {
+        response = {"error" : true,"message" : "Error adding data"};
+        } else {
+        response = {"error" : false,"message" : "Data added"};
+        }
+    res.json(response);
+      });
+    });
+
+router.get('/:id', function(req, res, next) {
+  //retrieve all lights from Monogo
+    lightSchema.findOne({'pin':"17"}, function (err, light) {
+          if (err) {
+              return console.error(err);
+          } else {
+              //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+              res.format({
+                //JSON response will show all blobs in JSON format
+                json: function(){
+                    res.json(light);
+                }
+            });
+          }
+    });
+});
+
+
+
+router.put('/:id', function(req, res, next) {
+  //retrieve all lights from Monogo
+    lightSchema.findOne({'pin':"17"}, function (err, light) {
+          if (err) {
+              return console.error(err);
+          } else {
+              //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+              res.format({
+                //JSON response will show all blobs in JSON format
+                json: function(){
+                    res.json(light);
                 }
             });
           }
